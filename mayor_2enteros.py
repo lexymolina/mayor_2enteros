f990d8f7630ed750d536c219c3ee1c01765da74e
sistemas <lexymato@colegioguanenta.edu.co>
2023-02-22 09:16:03 -0500

    se modifico el mayor_2enteros.py

--- mayor_2enteros.py
+++ mayor_2enteros.py
@@ -1,6 +1,21 @@
-#programa mayor 2 enteros
+#programa para verificar cual de dos enteros es el mayor
+
+print("-------------------------------")
+print("-----mayor 2 enteros-----")
+print("-------------------------------")
 
 # input
 x = int(input("digite el valor de x: "))
 y = int(input("digite el valor de y: "))
 
+#processing
+if x == y:
+    print("los numeros son iguales...")
+else:
+    if x > y:
+        mayor = x
+    else:
+        mayor = y        
+
+#output
+print("el mayor entre " + str(x) + " y " + str(y) + " es " + str(mayor))
